0a514cc33b823bd9c0b120e9e65ff084c2373b08
NanciMondragon <nantzamon@gmail.com>
2022-05-05 14:18:18 -0500

    Routing is performed in App.js, the first component is in Home (lack of functionality) style in CSS

---
+++ write-notes/src/components/Home.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+export function Home() {
+  
+    return (
+    
+    <div>
+        <div className='container'>
+          <img id='note'
+          src="https://imgur.com/x2eC1lG.jpeg" 
+          alt="Note" 
+          ></img>
+          </div>
+
+        <button class='btnGoogle'> Login whit Google </button>
+
+        <div className='footer'>
+        <h4>Write Notes Copyright 2022 by Nanci Mondragón <a class="gitHubLogo" href="https://github.com/NanciMondragon"
+           ><i class="fa-brands fa-github"></i> </a></h4>
+
+        </div>
+          
+
+
+    </div>
+  )
+  
+}
+
+export default Home
