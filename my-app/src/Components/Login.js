7b0e344519c389bf0182160ffc9af7cbd3335cf2
Lourdes Santiago <lourdes.santiago.palacios@gmail.com>
2022-05-11 05:59:45 -0500

    Con usuario logueado dirige a la ruta home

--- my-app/src/Components/Login.js
+++ my-app/src/Components/Login.js
@@ -1,15 +1,22 @@
 import React from "react";
 import { GoogleAuthProvider, signInWithPopup} from "firebase/auth";
 import {authentication} from "../firebase/firebaseConfig";
+import {useNavigate} from 'react-router-dom';
+
 
 
 function Login  () {
+    
+    const navigate = useNavigate();
+    const loginClick = ()=>{
+     navigate("/home");
+    }
     
    const signInWitGoogle =()=>{
        const provider =new GoogleAuthProvider();
        signInWithPopup(authentication, provider)
       .then((re) => {
-          console.log(re);
+          loginClick(re);
       }).catch((err) => {
           console.log(err);
       });
