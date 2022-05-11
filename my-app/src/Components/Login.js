d3765ac5dc042c5039ad01ab7d44ceda4b4eb40d
Lourdes Santiago <lourdes.santiago.palacios@gmail.com>
2022-05-11 03:21:28 -0500

    Se agrego Autenticacion con google

--- my-app/src/Components/Login.js
+++ my-app/src/Components/Login.js
@@ -1,7 +1,26 @@
 import React from "react";
+import { GoogleAuthProvider } from "firebase/auth";
+import {authentication} from "../firebase/firebaseConfig"
+import {signInWithPopup } from "firebase/auth";
 
-const Login = ()=> (
-    <button>Iniciar sesion con google</button>
-)
+
+function Login  () {
+    
+   const signInWitGoogle =()=>{
+       const provider =new GoogleAuthProvider();
+       signInWithPopup(authentication, provider)
+      .then((re) => {
+          console.log(re);
+      }).catch((err) => {
+          console.log(err);
+
+      });
+
+   }
+    return(
+        <button onClick={signInWitGoogle}>Iniciar sesion con google</button>
+
+    )
+}
 
 export default Login;
