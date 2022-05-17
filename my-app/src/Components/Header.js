377fef743fc18dd04dc3213ca3fd1206e2ed02e0
Lourdes Santiago <lourdes.santiago.palacios@gmail.com>
2022-05-17 01:57:48 -0500

    Cierra sesion del usuario

--- my-app/src/Components/Header.js
+++ my-app/src/Components/Header.js
@@ -1,23 +1,26 @@
+import { signOut } from "firebase/auth";
 import React from "react";
 // import { useNavigate } from "react-router-dom";
 // import { signOut } from "firebase/auth";
-// import { authentication } from "../lib/firebaseConfig"
+ import { authentication } from "../lib/firebaseConfig"
 
 function Header() {
     // const navigate = useNavigate();
     // const loginClick = () => {
     //     navigate("/");
     // }
-
-    // signOut(authentication).then((re) => {
-    //     console.log(re)
-    // }).catch((error) => {
-    //     // An error happened.
-    // });
+const logOut = ()=>{ 
+    signOut(authentication).then((re) => {
+        console.log(signOut)
+        console.log(re)
+    }).catch((error) => {
+        // An error happened.
+    });
+}
     return (
         <div className='registro'>
             <h2>Bienvenida a tus notas </h2>
-            <button className='logOut' >Cerrar sesión</button>
+            <button onClick={logOut} className='logOut' >Cerrar sesión</button>
         </div>
     )
 }
